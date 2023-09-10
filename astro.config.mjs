0eb608263ef89c648b94591c27e7af2949ea3e4f
Karel Soupa <karel.soupa@emz-hanauer.com>
2023-09-10 17:43:11 +0200

    pages

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+      site: 'https://karelSoupa.github.io',
+  base: '/qick-iframe-test',
+});
